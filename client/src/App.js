a5c8ce4ecd838b70ce4ba37b25f95e415b72c97c
Melissa Preece <melissanchez@gmail.com>
2021-06-28 12:02:31 -0700

    ISSUE: Data not updating
    Page is not re-rendering with fetched data

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
-// import { response } from 'express';
 // import React and useState, useEffect hook
 import React, { useState, useEffect} from 'react';
 import './App.css';
 
 /** Update the React App component (src/App.js file) to call the REST API to get a list of courses and render the results. **/
 function App() {
-  /* Steps to fetch data:*/
+  /* Steps to fetch data: */
   /// Store json response from courses API
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const courses = data;
 
-  // define useEffect to fetch API data: courses list
+  // Define useEffect to fetch API data: courses list
   useEffect(() => {
-    fetch('https://localhost:5000/api/courses') //Pass the URL to the fetch API.
-      .then((response) => response.json()) //Next we will parse the response object and extract the json response that is obtained.
-      .then((json) => setData(json)) //UI will display the data response.
-      .catch((error) => console.log(error)); //Catch any errors thrown from the fetch call.
+    fetch('http://localhost:5000/api/courses') //Pass the URL to the fetch API
+      .then((response) => response.json()) //Parse response to JSON
+      .then((data) => setData(data.courses)) //Set state to data parsed from JSON
+      .catch((error) => console.log(error)); //Catch any errors thrown from the fetch call
   }, []);
+
+  console.log(courses);
 
   return (
     // Mark up unordered list of courses
-    <div>
-      <ul>
-       {data.map((course, index) => (<li key={index}>{course}</li>))}
-      </ul>
+    <div>List of courses goes here.
+      <ul>{courses.map((course) => <li>{course}</li>)}</ul>
     </div>
   );
 }
