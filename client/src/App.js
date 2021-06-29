2eb2009f81b2cad84bc44fc346d83a08ebfb3a84
Melissa Preece <melissanchez@gmail.com>
2021-06-29 16:18:44 -0700

    FIXED API connects to React client

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,22 @@
 import React, { useState, useEffect} from 'react';
 import './App.css';
 
-/** Update the React App component (src/App.js file) to call the REST API to get a list of courses and render the results. **/
 function App() {
-  /* Steps to fetch data: */
-  /// Store json response from courses API
-  const [data, setData] = useState();
-  const courses = data;
+  // Define useState and store values
+  const [courses, setCourses] = useState([]);
 
   // Define useEffect to fetch API data: courses list
   useEffect(() => {
     fetch('http://localhost:5000/api/courses') //Pass the URL to the fetch API
       .then((response) => response.json()) //Parse response to JSON
-      .then((data) => setData(data.courses)) //Set state to data parsed from JSON
+      .then((courses) => setCourses(courses.courses)) //Update state to data fetched from API
       .catch((error) => console.log(error)); //Catch any errors thrown from the fetch call
   }, []);
 
-  console.log(courses);
-
+  // Mark up of unordered list of courses
   return (
-    // Mark up unordered list of courses
     <div>List of courses goes here.
-      <ul>{courses.map((course) => <li>{course}</li>)}</ul>
+      <ul>{courses.map((course) => <li key={course.id}>{course.title}</li>)}</ul>
     </div>
   );
 }
